b0982bb386bcc2c58543180735cb8ddad3606406
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2022-01-28 15:03:49 +0530

    fix: make wget quiet

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ chmod +x ./prereq.sh
 
 echo "Downloading image builder.."
 
-wget https://downloads.openwrt.org/snapshots/targets/ramips/mt7621/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
+wget -q https://downloads.openwrt.org/snapshots/targets/ramips/mt7621/openwrt-imagebuilder-ramips-mt7621.Linux-x86_64.tar.xz
 
 echo "Extracting image builder.."
 tar -J -x -f openwrt-imagebuilder-*.tar.xz
@@ -20,6 +20,8 @@ BASE_RM=-$(cat base_rm.list | tr '\n' ' -')
 BASE_ADD=$(cat base_add.list | tr '\n' ' ')
 
 PACKAGES="$BASE_ADD $BASE_RM"
+
+echo "Packages: " $PACKAGES
 
 PROFILE="tplink_archer-c6u-v1"
 
