1343bd3015967a8634755dd21702c49df982d1d7
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2022-04-07 13:50:36 +0530

    fix: c50: trigger_build: source parent trigger build to pass variables

--- c50/files/root/updater/trigger_build.sh
+++ c50/files/root/updater/trigger_build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-CMD="cd /root/updater/; LOCAL_VERSION=$LOCAL_VERSION; LATEST_SNAPSHOT=$LATEST_SNAPSHOT; DEVICE=ArcherC50v6; ./trigger_build.sh"
+CMD="cd /root/updater/; LOCAL_VERSION=$LOCAL_VERSION; LATEST_SNAPSHOT=$LATEST_SNAPSHOT; DEVICE=ArcherC50v6; . ./trigger_build.sh"
 ssh root@192.168.1.1 "$CMD"
